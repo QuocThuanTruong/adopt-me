833dbef577457e541431067d80c1bb8c082bafb5
QuocThuanTruong <lalalag129@gmail.com>
2021-01-20 16:49:57 +0700

    add setting file

---
+++ AdoptMe/Components/Main/UpdateMyProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UpdateMyProfileViewController.swift
+//  AdoptMe
+//
+//  Created by Quoc Thuan Truong on 1/20/21.
+//
+
+import UIKit
+
+class UpdateMyProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
